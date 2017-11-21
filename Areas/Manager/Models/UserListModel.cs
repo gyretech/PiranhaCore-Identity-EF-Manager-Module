aa32946b4b81cededc3a676090106dd8e7174d7e
gyretech <keshwar@gyretechnologies.com>
2017-11-21 11:03:36 -0500

    Initial Add
    Initial Add

---
+++ Areas/Manager/Models/UserListModel.cs
@@ -0,0 +1,29 @@
+﻿using Piranha.AspNetCore.Identity.EF;
+
+namespace Piranha.AspNetCore.Identity.EF.Manager.Areas.Manager.Models
+{
+    public class UserListModel
+    {
+        public UserListModel()
+        {
+            UserList = new PagedList<IdentityAppUser>();
+        }
+
+        #region Properties
+
+        public PagedList<IdentityAppUser> UserList { get; set; }
+
+        #endregion
+
+        public static UserListModel Get(AppUserRepository userRepository, string userId)
+        {
+            var model = new UserListModel();
+
+            model.UserList = userRepository.GetPage(1, 10);
+
+            return model;
+        }
+
+        
+    }
+}
